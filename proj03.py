b04849cf671c42c879ed9f1ba12825cd0879f095
Zamaneer <zamaneer@gmail.com>
2020-06-24 23:01:02 -0400

    Accompanies proj03library. File for testing functions

---
+++ proj03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 20:27:42 2020
+
+@author: nabilz
+"""
+# import 
+import proj03library
+
+print(proj03library.is_alpha("Aa1AA"))
+print(proj03library.is_digit("1aa"))
+print(proj03library.find_chr("stare","e"))
+print(proj03library.replace_chr("star", "a", "p"))
+print(proj03library.to_lower("STaR"))
+print(proj03library.to_upper("staAr"))
+print(proj03library.find_str("Star", "ar"))
+
