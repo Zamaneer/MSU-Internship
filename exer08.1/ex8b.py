f34031a8e077e04f7cc84a115ab02e72b21a9429
Zamaneer <zamaneer@gmail.com>
2020-07-03 22:33:51 -0400

    Cut down on excessive code, formatted to pep8 guidelines (mostly)

--- exer08.1/ex8b.py
+++ exer08.1/ex8b.py
@@ -2,11 +2,11 @@
     ###########################################################
     #  Exercise #8B
     #
-    #     Use the Deck and Card classes to play War 
+    #     Use the Deck and Card classes to play War
     #
     ###########################################################
-    
-# imports   
+ 
+# imports
 import cards
 
 # constants
@@ -17,10 +17,15 @@ the_deck = cards.Deck()
 the_deck.shuffle()
 
 
-def deal_cards():
+def deal_cards(to_deal):
     '''
     Deals 26 cards to each player and displays hands to terminal
-
+    
+    Parameters
+    ----------
+    to_deal : int
+        Number of cards to deal to each player
+        
     Returns
     -------
     player1_cards : list
@@ -31,30 +36,27 @@ def deal_cards():
 
     '''
     the_deck.shuffle()
-    
-    
+  
     # Deal 26 cards to each player (alternating)
-    player1_cards=[]
-    player2_cards=[]
-    for i in range(5):
+    player1_cards = []
+    player2_cards = []
+    for i in range(to_deal):
         player1_cards.append(the_deck.deal())
         player2_cards.append(the_deck.deal())
-    
+
     # Display initial cards
-    print( "===== Beginning cards =====" )
+    print("===== Beginning cards =====")
     print("")
     print("Player 1: ", player1_cards)
     print("")
     print("Player 2: ", player2_cards)
-    
-    
+
     return player1_cards, player2_cards
 
 
-
-def war(player1_cards, player2_cards):
+def war_round(player1_cards, player2_cards):
     '''
-    Goes through the entire game of war 
+    Goes through a round of war. Displays results to user and updates hands
 
     Parameters
     ----------
@@ -68,91 +70,80 @@ def war(player1_cards, player2_cards):
     None.
 
     '''
-    end = None
     
-    while not end: 
+    # Gets top card from each deck and sets up variables for winning
+    top_deck1 = player1_cards.pop(0)
+    top_deck2 = player2_cards.pop(0)
+    winner = ""
+
+    # Battle display: Shows "Battle" and drawn cards
+    print("""
+
+                          -----------------
+                               Battle
+                          -----------------
+          """)
+
+    print("Player 1: ", top_deck1)
+    print("Player 2: ", top_deck2)
+
+    # Processing for battle
+    # Handles cases for Ace on either player's side
+    if top_deck1.rank() == 1 and top_deck2.rank() != 1:
+        player1_cards.extend([top_deck1, top_deck2])
+        winner = "Player 1"
+
+    elif top_deck1.rank() != 1 and top_deck2.rank() == 1:
+        player2_cards.extend([top_deck2, top_deck1])
+        winner = "Player 2"
+
+    # Handles normal cases (cards drawn are not aces)
+    elif top_deck1.rank() > top_deck2.rank():
+        player1_cards.extend([top_deck1, top_deck2])
+        winner = "Player 1"
+
+    elif top_deck1.rank() < top_deck2.rank():
+        player2_cards.extend([top_deck2, top_deck1])
+        winner = "Player 2"
+
+    else:
+        player1_cards.append(top_deck1)
+        player2_cards.append(top_deck2)
+
+    # Determines what to say in the results string
+    if player1_cards == [] or player2_cards == []:
+        won = "game!"
+    else:
+        won = "battle!"
         
-        # Gets top card from each deck and sets up variables for winning
-        top_deck1 = player1_cards.pop(0)
-        top_deck2 = player2_cards.pop(0)
-        one_winnings = [top_deck1, top_deck2]
-        two_winnings = [top_deck2, top_deck1]
-        winner = ""
+    # Prints results of game/battle and hands
+    if winner != "":
+        print("{:^80}".format(winner + " wins the " + won))
+    else:
+        print("{:^80}".format("It's a tie!"))
+    print()
+    print("Player 1 hand: ", player1_cards)
+    print()
+    print("Player 2 hand: ", player2_cards)
         
-        
-        # Battle display: Shows "Battle" and drawn cards
-        print("""
-              
-                              -----------------
-                                   Battle
-                              -----------------
-              """)
-              
-        print("Player 1: ", top_deck1)
-        print("Player 2: ", top_deck2)
-        
-        
-        # Processing for battle
-        # Handles cases for Ace on either player's side
-        if top_deck1.rank() == 1 and top_deck2.rank() != 1:
-            player1_cards.extend(one_winnings)
-            winner = "Player 1"
-            
-        elif top_deck1.rank() != 1 and top_deck2.rank() == 1: 
-            player2_cards.extend(two_winnings)
-            winner = "Player 2"
-        
-        # Handles normal cases (cards drawn are not aces)
-        elif top_deck1.rank() > top_deck2.rank():
-            player1_cards.extend(one_winnings)
-            winner = "Player 1"
-            
-        elif top_deck1.rank() < top_deck2.rank():
-            player2_cards.extend(two_winnings)
-            winner = "Player 2"
-            
-        else:
-            player1_cards.append(top_deck1)
-            player2_cards.append(top_deck2)
-        
-        
-        # Determines if game is finished
-        if player1_cards == []:
-            end = "Yes"
-            won = "game!"
-        elif player2_cards == []:
-            end = "Yes"
-            won = "game!"
-        else:
-            won = "battle!"
-            
-        
-        # Prints results of game/battle and hands 
-        if winner != "":
-            print("{:^80}".format(winner + " wins the " + won))
-        else:
-            print("{:^80}".format("It's a tie!"))
-        print()
-        print("Player 1 hand: ", player1_cards)
-        print()
-        print("Player 2 hand: ", player2_cards)
-        
-        
+          
+def main():
+    
+    # Deal cards, enter number of cards to deal
+    player1_cards, player2_cards = deal_cards(26)
+    
+    # Game loop
+    end = None
+    while not end:
+        war_round(player1_cards, player2_cards)
         
         # User decision to continue or stop
-        end = input("Keep going (Anything other than return to stop)? ")
-          
-
-
-
-def main():
-    player1_cards, player2_cards = deal_cards()
-    war(player1_cards,player2_cards)
-    
-    
-    
-    
+        if player1_cards != [] and player2_cards != []:
+            end = input("End game (Press Enter to continue)? ")
+        else:
+            break
+        
 
 if __name__ == "__main__":
     main()
-
+    
